6c313ef96761ad2b74049eeee706150f430d2084
gennady <gennady@gennady-laptop>
2010-10-06 23:44:02 +0200

    Hello GAE!

---
+++ src/main/java/brix/demo/web/dav/SimpleRuntimeException.java
@@ -0,0 +1,19 @@
+package brix.demo.web.dav;
+/** 
+ * <b>Description:TODO</b>
+ * @author      gennady<br>
+ * <br>
+ * <b>Copyright:</b>     Copyright (c) 2006-2008 OXSEED AG <br>
+ * <b>Company:</b>       OXSEED AG  <br>
+ * 
+ * Creation:  Oct 4, 2010::9:51:08 PM<br> 
+ */
+public class SimpleRuntimeException extends RuntimeException {
+	SimpleRuntimeException(String s){
+		super(s);
+	}
+
+}
+
+
+ 
